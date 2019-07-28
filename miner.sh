af42857bbd0363f5ae0c31f5b71708753610e76d
k4zh4r <49700481+k4zh4r@users.noreply.github.com>
2019-07-28 13:40:13 -0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -3,8 +3,7 @@ apt-get install libcurl4-openssl-dev git
 apt-get install  build-essential
 apt-get install autotools-dev autoconf
 apt-get install libcurl3 libcurl4-gnutls-dev
-mkdir /home/cpu-miner
-cd /home/cpu-miner
+cd /home/
 git clone https://github.com/pooler/cpuminer
 cd cpuminer
 ./autogen.sh
