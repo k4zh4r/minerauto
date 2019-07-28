355cb7d46c7066517a4cf400f4e3711edc5850d0
k4zh4r <49700481+k4zh4r@users.noreply.github.com>
2019-07-28 20:18:30 -0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,6 +1,7 @@
  sudo su
- apt-get update
- mkdir /home/miner
- cd /home/miner
- wget https://uc50aaf72fecc31a8eebc4901b3b.dl.dropboxusercontent.com/cd/0/get/Ali3ZN0DNNpGjdHWxmqWH0iKGtuTHDTfR9FizjwqoxKx9oFvduXLPUSBdeHp6AUIBK_V4VTAGdv--lBZu-bT06csOxPDCcCugDAMMlittDxw1A/file?dl=1#
- dpkg -i minergate-cli.deb
+apt-get update
+apt-get install software-properties-common
+add-apt-repository ppa:ethereum/ethereum
+apt-get update
+apt-get install ethminer
+
